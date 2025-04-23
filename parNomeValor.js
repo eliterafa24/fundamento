d419cdb62028ae3a99e720895b37d8a9d5d4d82c
Rafael <eliterafa9@gmail.com>
2025-04-23 17:48:56 -0300

    fiz mas para experiencia

---
+++ parNomeValor.js
@@ -0,0 +1,20 @@
+const saudacao = 'Opa'
+
+function exec() {
+    const saudacao = 'Falaaa'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Rafael',
+    idade: 34,
+    peso: 65,
+    endereco: {
+        logradouro: 'rua bem legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
